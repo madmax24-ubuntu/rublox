5105fd0b74db4b3f9c7e1da162e3c985e023f5b4
madmax24-ubuntu <makskachmarik321@gmail.com>
2026-05-23 15:36:59 +0600

    auto-backup: pre-edit snapshot

--- world/MapGenerator.js
+++ world/MapGenerator.js
@@ -1398,7 +1398,7 @@ const gapConfigs = [
             const len = Math.sqrt(dx * dx + dz * dz);
             const angle = Math.atan2(dz, dx);
 
-            const pathGeo = new THREE.PlaneGeometry(6, len, 16, 16);
+            const pathGeo = new THREE.PlaneGeometry(6, len, 8, 8);
             const path = new THREE.Mesh(pathGeo, pathMat);
             path.rotation.x = -Math.PI / 2;
             path.rotation.z = -angle;
