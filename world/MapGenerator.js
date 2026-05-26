7fd732cbe7132658cd1f913e6e365ed9e8ec84f0
madmax24-ubuntu <makskachmarik321@gmail.com>
2026-05-26 12:04:27 +0600

    auto-backup: pre-edit snapshot

--- world/MapGenerator.js
+++ world/MapGenerator.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { mergeBufferGeometries } from "three/examples/jsm/utils/BufferGeometryUtils.js";
 import { MapGenerator as TileMapGenerator } from "./MapGeneratorNode.js";
 
 // ============ NOISE UTILITY ============
