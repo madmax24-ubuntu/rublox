59bde64a857e3af3f65be41b8dc1127c87c72fe7
madmax24-ubuntu <makskachmarik321@gmail.com>
2026-05-27 20:38:41 +0600

    auto-backup: pre-edit snapshot

--- main.js
+++ main.js
@@ -491,8 +491,10 @@ class Game {
         this.entityManager = new EntityManager(this.scene);
         this.entityManager.physicsRef = this.physics;
         this.scene.userData.entityManager = this.entityManager;
+        console.log('[Game] EntityManager created');
         this.lootManager = new LootManager(this.scene, this.map);
         this.lootManager.generateChests?.();
+        console.log('[Game] LootManager created');
 
         const spawnPads = this.map.getSpawnPads?.() || [];
         this.player = new Player(this.scene, this.camera, this.input);
