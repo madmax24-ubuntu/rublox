e8c3a4ffe1c5416e30c53772062d5490a0e10448
madmax24-ubuntu <makskachmarik321@gmail.com>
2026-05-28 01:16:22 +0600

    auto-backup: pre-edit snapshot

--- world/MapGenerator.js
+++ world/MapGenerator.js
@@ -2274,36 +2274,35 @@ export class MapGenerator {
         await this.buildBridges();
         console.log('[MapGen] bridges done');
 
-        console.log('[MapGen] yielding 300ms...');
-        await new Promise(r => setTimeout(r, 300));
+        console.log('[MapGen] yielding 500ms before outposts...');
+        await new Promise(r => setTimeout(r, 500));
 
         await this.buildOuterOutposts();
         console.log('[MapGen] outer outposts done');
         await this.buildHazardZones();
         console.log('[MapGen] hazard zones done');
-
-        console.log('[MapGen] yielding 100ms...');
-        await new Promise(r => setTimeout(r, 100));
+        await new Promise(r => setTimeout(r, 500));
 
         await this.buildLootClusters();
         console.log('[MapGen] loot clusters done');
         await this.buildFirePits();
         console.log('[MapGen] fire pits done');
         this.reportProgress(0.75, 'Объекты размещены');
+        await new Promise(r => setTimeout(r, 500));
 
         await this.buildParticleSystems();
         console.log('[MapGen] particle systems done');
         this.reportProgress(0.82, 'Частицы');
-        await new Promise(r => setTimeout(r, 50));
+        await new Promise(r => setTimeout(r, 500));
         await this.buildTraps();
         console.log('[MapGen] traps done');
-        await new Promise(r => setTimeout(r, 50));
+        await new Promise(r => setTimeout(r, 500));
         await this.buildFogZones();
         console.log('[MapGen] fog zones done');
-        await new Promise(r => setTimeout(r, 50));
+        await new Promise(r => setTimeout(r, 500));
         await this.buildRadiationZones();
         console.log('[MapGen] radiation zones done');
-        await new Promise(r => setTimeout(r, 50));
+        await new Promise(r => setTimeout(r, 500));
         await this.buildLootData();
         console.log('[MapGen] loot data done');
         this.reportProgress(0.90, 'Лут и ловушки');
