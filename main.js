85e961c3bb844189e7bca69328d7b7e079f8d237
madmax24-ubuntu <makskachmarik321@gmail.com>
2026-05-28 17:54:51 +0600

    auto-backup: pre-edit snapshot

--- main.js
+++ main.js
@@ -501,7 +501,7 @@ class Game {
         console.log('[Game] lootManager:', typeof this.lootManager, !!this.lootManager);
         console.log('[Game] generateChests:', typeof this.lootManager?.generateChests);
         try {
-            const result = await this.lootManager.generateChests?.();
+            const result = await this.lootManager.generateChests();
             console.log('[Game] chests generated, result:', typeof result);
         } catch (err) {
             console.warn('[Game] generateChests failed:', err.message);
