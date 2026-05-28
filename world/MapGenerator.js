8e4c9091df72325c2e6ad3d91e9bcb7084d857be
madmax24-ubuntu <makskachmarik321@gmail.com>
2026-05-28 23:11:37 +0600

    auto-backup: pre-edit snapshot

--- world/MapGenerator.js
+++ world/MapGenerator.js
@@ -82,7 +82,7 @@ const COLOR = {
     ruinMoss: 0x7a9a5a,
     crystalBlue: 0x4488cc,
     crystalPurple: 0x8844aa,
-    crystalFloor: 0x5a5a6a,
+    crystalFloor: 0x0088cc,
     crystalReflect: 0x99bbdd,
     crystalGlow: 0x88ccff,
     lava: 0xff4400,
