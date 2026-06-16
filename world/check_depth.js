258dad70a52d354a541b51c68f4442b6c41e175c
madmax24-ubuntu <makskachmarik321@gmail.com>
2026-06-17 04:53:16 +0600

    auto-backup: pre-edit snapshot

--- world/check_depth.js
+++ world/check_depth.js
@@ -28,33 +28,3 @@ for (let i = 1; i <= Math.min(942, src.split('\n').length); i++) {
         else if (ch === '}') bd--;
     }
 }
-
-// Print depth at every closing brace before _addTree
-bd = 0;
-for (let i = 1; i <= Math.min(942, src.split('\n').length); i++) {
-    const line = src.split('\n')[i-1];
-    for (const ch of line) {
-        if (ch === '{') bd++;
-        else if (ch === '}') bd--;
-    }
-}
-
-// Print depth at every closing brace before _addTree
-bd = 0;
-for (let i = 1; i <= Math.min(942, src.split('\n').length); i++) {
-    const line = src.split('\n')[i-1];
-    for (const ch of line) {
-        if (ch === '{') bd++;
-        else if (ch === '}') bd--;
-    }
-}
-
-// Print depth at every closing brace before _addTree
-bd = 0;
-for (let i = 1; i <= Math.min(942, src.split('\n').length); i++) {
-    const line = src.split('\n')[i-1];
-    for (const ch of line) {
-        if (ch === '{') bd++;
-        else if (ch === '}') bd--;
-    }
-}
